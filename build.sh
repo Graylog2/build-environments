99b724dbd21cc74f142f405b78b9a85ced2a31fc
Marius Sturm <marius@graylog.com>
2015-02-10 20:18:35 +0100

    add base environments

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+export DOCKER_HOST=${DOCKER_HOST:-'tcp://localhost:4243'}
+
+for DISTRIBUTION in */
+do
+  for CONTAINER_NAME in ${DISTRIBUTION}*
+  do
+    if [ -d "${CONTAINER_NAME}" ]; then
+      LEGACY_ID=$(docker images | grep $CONTAINER_NAME | awk '{print $3}')
+      if [[ ! -z "$LEGACY_ID" ]]; then
+        echo "Delete existing image ${LEGACY_ID} - ${CONTAINER_NAME}"
+        docker rmi -f ${CONTAINER_NAME}
+      fi
+      echo "Building environment for ${CONTAINER_NAME}"
+      docker build --force-rm=true -t ${CONTAINER_NAME} ${CONTAINER_NAME}
+    fi;
+  done
+done
