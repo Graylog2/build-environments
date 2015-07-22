703cce7a638e504ef85dc806c4317119c437f25f
Marius Sturm <marius@graylog.com>
2015-07-22 11:42:27 +0200

    dont kill jenkins connection on exit

--- ubuntu/integration-environment/run.sh
+++ ubuntu/integration-environment/run.sh
@@ -19,7 +19,7 @@ fi
 mkdir -p /opt/graylog/server
 tar xzvfC $LATEST /opt/graylog/server --strip 1
 
-/sbin/my_init &
+/sbin/my_init --no-kill-all-on-exit &
 
 echo "Waiting for Graylog server"
 until $(curl --output /dev/null --silent --head --fail http://localhost:12900/api-browser); do
