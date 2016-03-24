731573e1bcd1c77f32bf1fbc416e63e95155293b
Marius Sturm <marius@graylog.com>
2016-03-24 18:00:39 +0100

    fix path issues

--- build.sh
+++ build.sh
@@ -3,10 +3,17 @@
 set -e
 export DOCKER_HOST=${DOCKER_HOST:-'unix:///var/run/docker.sock'}
 
+# Copy files, we don't want to download all the time, to Docker context
+files=( /usr/share/phantomjs/latest/bin/phantomjs )
+
 for DISTRIBUTION in */
 do
   for CONTAINER_NAME in ${DISTRIBUTION}*
   do
+    for file in "${files[@]}"
+    do
+      cp "$file" .
+    done
     if [ -d "${CONTAINER_NAME}" ]; then
       LEGACY_ID=$(docker images | grep $CONTAINER_NAME | awk '{print $3}')
       if [[ ! -z "$LEGACY_ID" ]]; then
