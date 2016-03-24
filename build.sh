0ebf7b12c2be1d4bf09185674f55e984b76f2b8f
Marius Sturm <marius@graylog.com>
2016-03-24 18:04:27 +0100

    copy binaries to correct directory

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ do
   do
     for file in "${files[@]}"
     do
-      cp "$file" .
+      cp ${file} "${DISTRIBUTION}/${CONTAINER_NAME}/"
     done
     if [ -d "${CONTAINER_NAME}" ]; then
       LEGACY_ID=$(docker images | grep $CONTAINER_NAME | awk '{print $3}')
