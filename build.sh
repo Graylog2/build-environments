2366468c5508bbf4b6454fe74d895453676b2768
Marius Sturm <marius@graylog.com>
2016-05-27 13:44:16 +0200

    continue if image can be deleted

--- build.sh
+++ build.sh
@@ -18,7 +18,7 @@ do
       LEGACY_ID=$(docker images | grep $CONTAINER_NAME | awk '{print $3}')
       if [[ ! -z "$LEGACY_ID" ]]; then
         echo "Delete existing image ${LEGACY_ID} - ${CONTAINER_NAME}"
-        docker rmi -f ${CONTAINER_NAME}
+        docker rmi -f ${CONTAINER_NAME} || true
       fi
       echo "Building environment for ${CONTAINER_NAME}"
       docker build --force-rm=true -t ${CONTAINER_NAME} ${CONTAINER_NAME}
